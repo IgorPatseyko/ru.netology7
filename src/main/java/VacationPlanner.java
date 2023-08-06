11e2782994684fd596637fe1c0c58cdd6080853a
igor test <igor.patseyko@mail.ru>
2023-08-06 12:45:35 +0200

    Homework 5.1

---
+++ src/main/java/VacationPlanner.java
@@ -0,0 +1,20 @@
+package ru.netology.services;
+
+public class VacationPlanner {
+    public int calculateVacationMonths(int income, int expenses, int threshold) {
+        int vacationMonths = 0; // счетчик месяцев отдыха
+        int savings = 0; // количество денег на счету
+
+        for (int month = 0; month < 12; month++) {
+            if (savings >= threshold) { // можем ли отдыхать?
+                vacationMonths++; // увеличиваем счетчик месяцев отдыха
+                int vacationExpenses = expenses * 3;
+                savings = savings - vacationExpenses;
+            } else {
+                savings = savings + income - expenses;
+            }
+        }
+
+        return vacationMonths;
+    }
+}
---
+++ src/test/java/VacationPlannerTest.java
@@ -0,0 +1,34 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VacationPlannerTest {
+
+    @Test
+    public void testVacationPlannerExample1() {
+        VacationPlanner planner = new VacationPlanner();
+        int income = 10000;
+        int expenses = 3000;
+        int threshold = 20000;
+        int expectedVacationMonths = 3;
+
+        int actualVacationMonths = planner.calculateVacationMonths(income, expenses, threshold);
+
+        assertEquals(expectedVacationMonths, actualVacationMonths);
+    }
+
+    @Test
+    public void testVacationPlannerExample2() {
+        VacationPlanner planner = new VacationPlanner();
+        int income = 100000;
+        int expenses = 60000;
+        int threshold = 150000;
+        int expectedVacationMonths = 2;
+
+        int actualVacationMonths = planner.calculateVacationMonths(income, expenses, threshold);
+
+        assertEquals(expectedVacationMonths, actualVacationMonths);
+    }
+}
